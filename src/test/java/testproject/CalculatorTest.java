b8a44e1ed224361807acdefffd0193d338cacb92
Alina-Arta <63864857+Alina-Arta@users.noreply.github.com>
2020-12-15 14:24:51 +0300

    Update CalculatorTest.java

--- src/test/java/testproject/CalculatorTest.java
+++ src/test/java/testproject/CalculatorTest.java
@@ -5,18 +5,10 @@ import static org.assertj.core.api.Assertions.*;
 
 public class CalculatorTest {
 
-    @Test
-    public void testSum() {
-        assertThat(Calculator.sum(2, 2)).isEqualTo(4);
-    }
-
+    
     @Test
     public void testSubtract() {
         assertThat(Calculator.subtract(3,2)).isEqualTo(1);
     }
 
-    @Test
-    public void testSum2() {
-        assertThat(Calculator.sum(3, 2)).isEqualTo(4);
-    }
 }
